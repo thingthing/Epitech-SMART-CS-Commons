ca3ebbdc41cfc15fd0d71cafcdf463fe879e879f
Vincent Buresi <vincent.buresi@gmail.com>
2014-10-02 06:56:18 -0400

    Added test file for cs commons

---
+++ src/eip/smart/cscommons/CSCommons.java
@@ -0,0 +1,11 @@
+package eip.smart.cscommons;
+
+/**
+ * Created by vincent buresi on 10/2/14.
+ */
+@SuppressWarnings("unused")
+public class CSCommons {
+    public static String getVersion() {
+        return "0.1b";
+    }
+}
