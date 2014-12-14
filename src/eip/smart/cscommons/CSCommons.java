d429318d84abed240bdcf591102d0449169fd7dd
Pierre Demessence <pierredemessence@gmail.com>
2014-12-14 10:48:31 +0100

    Fixed warnings

--- src/eip/smart/cscommons/CSCommons.java
+++ src/eip/smart/cscommons/CSCommons.java
@@ -1,66 +1,66 @@
 package eip.smart.cscommons;
 
+import java.io.IOException;
+
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
-
 /**
  * Created by vincent buresi on 10/2/14.
  */
-@SuppressWarnings("unused")
 public class CSCommons {
-    public final static String PROJECT_VERSION = "0.1b";
-    public final static String JSON_VERSION = "2.4.3";
+	private static class JSONTestObject {
+		public String	jsonVersion	= CSCommons.JSON_VERSION;
+		public String	jsonJarName	= "jackson-core-" + CSCommons.JSON_VERSION + ".jar";
 
-    private static class JSONTestObject {
-        public String jsonVersion = JSON_VERSION;
-        public String jsonJarName = "jackson-core-" + JSON_VERSION + ".jar";
-        public JSONTestObject() {}
-    }
+		public JSONTestObject() {}
+	}
 
-    public static String getVersion() {
-        return PROJECT_VERSION;
-    }
+	public static String getJacksonVersion() {
+		return CSCommons.JSON_VERSION;
+	}
 
-    public static String getJacksonVersion() {
-        return JSON_VERSION;
-    }
+	public static String getVersion() {
+		return CSCommons.PROJECT_VERSION;
+	}
 
-    public static String testJacksonVersion() {
-        ObjectMapper mapper = new ObjectMapper();
-        JSONTestObject inputObject = new JSONTestObject();
+	public static String testJacksonVersion() {
+		ObjectMapper mapper = new ObjectMapper();
+		JSONTestObject inputObject = new JSONTestObject();
 
-        String jsonOutput = null;
-        try {
+		String jsonOutput = null;
+		try {
 
-            jsonOutput = mapper.writeValueAsString(inputObject);
+			jsonOutput = mapper.writeValueAsString(inputObject);
 
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
 
-        String jsonVersion = null;
-        String jsonJarName = null;
+		String jsonVersion = null;
+		String jsonJarName = null;
 
-        try {
+		try {
 
-            JSONTestObject outputObject = mapper.readValue(jsonOutput, JSONTestObject.class);
-            jsonVersion = outputObject.jsonVersion;
-            jsonJarName = outputObject.jsonJarName;
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
-        } catch (JsonParseException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			JSONTestObject outputObject = mapper.readValue(jsonOutput, JSONTestObject.class);
+			jsonVersion = outputObject.jsonVersion;
+			jsonJarName = outputObject.jsonJarName;
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        if (jsonVersion != null && jsonJarName != null)
-            return "JSON test :\nJSON String : " + jsonOutput + "\nJSON object version : " + jsonVersion + " jarName : " + jsonJarName;
-        else
-            return "JSON test failure ! Please check your project settings.";
-    }
+		if (jsonVersion != null && jsonJarName != null)
+			return "JSON test :\nJSON String : " + jsonOutput + "\nJSON object version : " + jsonVersion + " jarName : " + jsonJarName;
+		return "JSON test failure ! Please check your project settings.";
+	}
+
+	public final static String	PROJECT_VERSION	= "0.1b";
+
+	public final static String	JSON_VERSION	= "2.4.3";
 }
