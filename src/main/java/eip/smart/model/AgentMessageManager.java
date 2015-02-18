951a873b55f311b9933e18ddfccb858399fff2ca
Pierre Demessence <pierredemessence@gmail.com>
2015-02-18 12:01:30 +0100

    fixed warnings

--- src/main/java/eip/smart/model/AgentMessageManager.java
+++ src/main/java/eip/smart/model/AgentMessageManager.java
@@ -8,10 +8,10 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
 public class AgentMessageManager {
-	private HashMap<String, AgentMessageHandler>	handlers	= new HashMap<>();
+	private HashMap<String, AgentMessageHandler<?>>	handlers	= new HashMap<>();
 	private char									separator	= ':';
 
-	public void addHandler(String key, AgentMessageHandler handler) {
+	public void addHandler(String key, AgentMessageHandler<?> handler) {
 		this.handlers.put(key, handler);
 	}
 
@@ -22,7 +22,7 @@ public class AgentMessageManager {
 		}
 		String key = msg.substring(0, msg.indexOf(this.separator));
 		String data = msg.substring(msg.indexOf(this.separator) + 1, msg.length());
-		for (Entry<String, AgentMessageHandler> entry : this.handlers.entrySet())
+		for (Entry<String, AgentMessageHandler<?>> entry : this.handlers.entrySet())
 			if (entry.getKey().equals(key)) {
 				entry.getValue().handleMessage(data, agent);
 				return;
