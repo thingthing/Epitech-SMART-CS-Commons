bd129e920308e27f9b89e3a919e643f81ce80339
adrien rouchy <rouchy.adrien@gmail.com>
2015-02-28 20:42:42 +0100

    correction still agent status bis

--- src/main/java/eip/smart/model/Agent.java
+++ src/main/java/eip/smart/model/Agent.java
@@ -173,16 +173,15 @@ public class Agent implements Serializable {
 
 	public void updateState() {
 		boolean still = true;
+		int check_size = 10;
 		int i = 0;
-		if (this.getPositions().size() > 3)
-		{
-			while (i < ((this.getPositions().size() > 5) ? 5 : this.getPositions().size()))
+		
+			while (i < ((this.getPositions().size() > check_size) ? check_size : this.getPositions().size()))
 			{
 				if (this.getCurrentPosition() != this.getPositions().get(i) && still)
 					still = false;
 				i++;
 			}
-		}	
 		if (Date.from(Instant.now()).getTime() - this.lastContact.getTime() > 5 * 60 * 1000)
 			this.state = AgentState.LOST;
 		else if (still)
