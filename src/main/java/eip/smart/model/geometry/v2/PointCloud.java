5bdcc5c0f9582a77d771a749c3d748d7b79e833b
Pierre Demessence <pierredemessence@gmail.com>
2015-08-08 19:23:00 +0200

    Updated PointCloud with some empty methods for the future.

--- src/main/java/eip/smart/model/geometry/v2/PointCloud.java
+++ src/main/java/eip/smart/model/geometry/v2/PointCloud.java
@@ -8,14 +8,39 @@ import org.apache.commons.math3.ml.clustering.DBSCANClusterer;
 
 public abstract class PointCloud<T extends Point> {
 
-	protected List<T>	points	= new ArrayList<>();
+	// TODO use configuration
+	private static final double	DBSCAN_DEFAULT_EPS		= 80;
+	private static final int	DBSCAN_DEFAULT_MINPTS	= 3;
+
+	private List<T>				points					= new ArrayList<>();
+
+	public void add(List<T> points) {
+		for (T point : points)
+			this.points.add(point);
+	}
 
 	public void add(T point) {
 		this.points.add(point);
 	}
 
 	public List<Cluster<T>> findClusters() {
-		DBSCANClusterer<T> clusterer = new DBSCANClusterer<>(10, 2);
+		return this.findClusters(PointCloud.DBSCAN_DEFAULT_EPS, PointCloud.DBSCAN_DEFAULT_MINPTS);
+	}
+
+	/**
+	 * @param eps
+	 *            maximum radius of the neighborhood to be considered
+	 * @param minPts
+	 *            minimum number of points needed for a cluster
+	 * @see DBSCANClusterer#DBSCANClusterer(double, int)
+	 */
+	public List<Cluster<T>> findClusters(double eps, int minPts) {
+		DBSCANClusterer<T> clusterer = new DBSCANClusterer<>(eps, minPts);
 		return clusterer.cluster(this.points);
+	}
+
+	public List<T> getPoints() {
+		// TODO Create a copy so it can't be modified.
+		return this.points;
 	}
 }
--- src/main/java/eip/smart/model/geometry/v2/PointCloud2D.java
+++ src/main/java/eip/smart/model/geometry/v2/PointCloud2D.java
@@ -11,23 +11,28 @@ import org.apache.commons.math3.geometry.euclidean.twod.hull.MonotoneChain;
 
 public class PointCloud2D extends PointCloud<Point2D> {
 	public static void main(String[] args) {
-		PointCloud2D pc2d = new PointCloud2D();
-		pc2d.add(new Point2D(0, 0));
-		pc2d.add(new Point2D(5, 5));
-		pc2d.add(new Point2D(0, 5));
-		pc2d.add(new Point2D(5, 0));
-		pc2d.add(new Point2D(2.5, 2.5));
-		pc2d.add(new Point2D(-5, -5));
-		pc2d.add(new Point2D(8, 9));
-		pc2d.add(new Point2D(8, 6));
-		ConvexHull2D hull2d = pc2d.generateConvexHull2D();
+		PointCloud2D pcl2d = new PointCloud2D();
+		pcl2d.add(new Point2D(0, 0));
+		pcl2d.add(new Point2D(5, 5));
+		pcl2d.add(new Point2D(0, 5));
+		pcl2d.add(new Point2D(5, 0));
+		pcl2d.add(new Point2D(2.5, 2.5));
+		pcl2d.add(new Point2D(-5, -5));
+		pcl2d.add(new Point2D(8, 9));
+		pcl2d.add(new Point2D(8, 6));
+		ConvexHull2D hull2d = pcl2d.generateConvexHull2D();
 		System.out.println(Arrays.deepToString(hull2d.getVertices()));
 		for (Segment segment : hull2d.getLineSegments())
 			System.out.println(segment.getStart() + " -> " + segment.getEnd());
 	}
 
+	/**
+	 * TODO find/create the algorithm
+	 */
+	public void generateConcaveHull2D() {}
+
 	public ConvexHull2D generateConvexHull2D() {
 		ConvexHullGenerator2D hullGenerator2D = new MonotoneChain();
-		return hullGenerator2D.generate(new ArrayList<Vector2D>(this.points));
+		return hullGenerator2D.generate(new ArrayList<Vector2D>(this.getPoints()));
 	}
 }
