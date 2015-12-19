7d3d85a55976af42678a8b9589a9d9d73415bbc0
Pierre Demessence <pierredemessence@gmail.com>
2015-12-19 19:04:31 +0100

    Point3D now have Color. Color received and decoded from UDP. (Forgot File)

---
+++ src/main/java/eip/smart/cscommons/model/geometry/Color.java
@@ -0,0 +1,67 @@
+package eip.smart.cscommons.model.geometry;
+
+import java.awt.color.ColorSpace;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import eip.smart.cscommons.model.JSONViews;
+
+public class Color extends java.awt.Color {
+
+	public Color(ColorSpace cspace, float[] components, float alpha) {
+		super(cspace, components, alpha);
+	}
+
+	public Color(float r, float g, float b) {
+		super(r, g, b);
+	}
+
+	public Color(float r, float g, float b, float a) {
+		super(r, g, b, a);
+	}
+
+	public Color(int rgb) {
+		super(rgb);
+	}
+
+	public Color(int rgba, boolean hasalpha) {
+		super(rgba, hasalpha);
+	}
+
+	public Color(int r, int g, int b) {
+		super(r, g, b);
+	}
+
+	public Color(int r, int g, int b, int a) {
+		super(r, g, b, a);
+	}
+
+	public Color(java.awt.Color color) {
+		this(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha());
+	}
+
+	@JsonView({ JSONViews.HTTP.class })
+	@Override
+	public int getAlpha() {
+		return super.getAlpha();
+	}
+
+	@JsonView({ JSONViews.HTTP.class })
+	@Override
+	public int getBlue() {
+		return super.getBlue();
+	}
+
+	@JsonView({ JSONViews.HTTP.class })
+	@Override
+	public int getGreen() {
+		return super.getGreen();
+	}
+
+	@JsonView({ JSONViews.HTTP.class })
+	@Override
+	public int getRed() {
+		return super.getRed();
+	}
+
+}
